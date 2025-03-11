b073ad607ecced090a6f7b08fcafcb4925335201
Calvin <calvin71203@gmail.com>
2025-03-11 02:10:10 -0400

    Save

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,24 +1,35 @@
 return {
-        "nvimtools/none-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-                local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local null_ls = require("null-ls")
 
-                null_ls.setup({
-                        sources = {
-                                null_ls.builtins.formatting.stylua, -- Lua formatter
-                                null_ls.builtins.formatting.prettier, -- JS, TS, HTML formatter
-                                null_ls.builtins.completion.spell, -- Spell checker
-                                --null_ls.builtins.formatting.clang_format, -- C++, C
-                                -- null_ls.builtins.formatting.rustfmt, -- Rust
-                        },
-                })
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua, -- Lua formatter
+                null_ls.builtins.formatting.prettier, -- JS, TS, HTML formatter
+                null_ls.builtins.completion.spell, -- Spell checker
+                -- null_ls.builtins.formatting.clang_format, -- C++, C
+                -- null_ls.builtins.formatting.rustfmt, -- Rust
+            },
+            on_attach = function(client, bufnr)
+                local filetype = vim.bo[bufnr].filetype
 
-                -- Auto format on save
-                vim.api.nvim_create_autocmd("BufWritePre", {
+                -- Prevent `none-ls` from formatting Zig files
+                if filetype == "zig" then
+                    return
+                end
+
+                -- Run formatting only if `none-ls` supports it
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
                         callback = function()
-                                vim.lsp.buf.format({ async = false })
+                            vim.lsp.buf.format({ async = false })
                         end,
-                })
-        end,
+                    })
+                end
+            end,
+        })
+    end,
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,34 +1,52 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "clangd", "gopls", "rust_analyzer" },
-		})
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "clangd", "gopls", "rust_analyzer", "zls" },
+        })
 
-		local lspconfig = require("lspconfig")
+        local lspconfig = require("lspconfig")
 
-		-- Lua setup (fix undefined `vim` warning)
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
+        -- Lua setup (fix undefined `vim` warning)
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
 
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		-- C++ setup
-		lspconfig.clangd.setup({ capabilities = capabilities })
+        -- C++ setup
+        lspconfig.clangd.setup({ capabilities = capabilities })
 
-		-- Golang setup
-		lspconfig.gopls.setup({ capabilities = capabilities })
+        -- Golang setup
+        lspconfig.gopls.setup({ capabilities = capabilities })
 
-		-- Rust setup
-		lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-	end,
+        -- Rust setup
+        lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+
+        -- Fixed Zig setup
+        lspconfig.zls.setup({
+            capabilities = capabilities,
+            settings = {
+                zls = { -- This should be inside `zls`, NOT `Zig`
+                    enable_autofix = true,
+                },
+            },
+        })
+
+        -- Ensure auto-formatting on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.zig",
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+            end,
+        })
+    end,
 }
